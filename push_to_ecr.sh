6a41ea032b2ca3e32afe0ac9e231b2007602a595
Ray Ng <ray.ng@gradeous.com.au>
2019-09-05 17:36:43 +1000

    put version tag for docker image

--- push_to_ecr.sh
+++ push_to_ecr.sh
@@ -5,3 +5,4 @@ aws ecr get-login --no-include-email --region us-west-1 | sh
 
 echo "Pushing image to ECR !!!!!!!"
 docker push 578612111946.dkr.ecr.us-west-1.amazonaws.com/hashicorp-demo:latest
+docker push 578612111946.dkr.ecr.us-west-1.amazonaws.com/hashicorp-demo:$VERSION
